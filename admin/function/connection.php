8db48f1e166a94e7cb0f7fede6f29389a29942e9
blackcoffelate <juli.iot@ubl.ac.id>
2021-06-28 09:56:27 +0700

    update 280621

--- admin/function/connection.php
+++ admin/function/connection.php
@@ -3,7 +3,7 @@
 $host   = "localhost";
 $uname  = "root";
 $pswd   = "";
-$dbname = "p_tekno_inventaris";
+$dbname = "p_tekno_inventaris_0621";
 
 $conn = new mysqli($host, $uname, $pswd, $dbname) or die("Failed to connect database".$conn->error);
 
